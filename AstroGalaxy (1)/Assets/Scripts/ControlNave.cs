7b7b166742feed8eae8f43e0589efaa0aa6795b4
Gustavo-Tovar <Gustavo-Tovar@users.noreply.github.com>
2020-05-18 18:40:28 -0500

    Creación Rayo, Fondo, falta el script del rayo porque mi pc esta lento

---
+++ AstroGalaxy (1)/Assets/Scripts/ControlNave.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlNave : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("left"))
+        {
+            gameObject.transform.Translate(-20f * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("right"))
+        {
+            gameObject.transform.Translate(20f * Time.deltaTime, 0, 0);
+        }
+
+    }
+}
