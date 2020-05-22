44b2fb6373b4693f4bf574df1410e3831ae44714
mateoOrdonez <60628762+mateoOrdonez@users.noreply.github.com>
2020-05-22 12:53:25 -0500

    Arreglo de la nave
    Se mueve con wasd, tanto en x como en z, se le agrego una inclinación al moverse y la nave no se sale del limite del mapa

--- AstroGalaxy (1)/Assets/Scripts/ControlNave.cs
+++ AstroGalaxy (1)/Assets/Scripts/ControlNave.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class ControlNave : MonoBehaviour
 {
+    [Header("ControlNave")]
+    public float velocidadNave = 20;
+    Rigidbody rigidNave;
+    Vector3 movimiento;
+
+
     [Header("Disparo")]
     public GameObject disparo;
     public Transform posdisparo;
@@ -12,26 +18,43 @@ public class ControlNave : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        rigidNave = GetComponent<Rigidbody>();
         tiempodisparo = 0.4f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("left"))
-        {
-            gameObject.transform.Translate(-20f * Time.deltaTime, 0, 0);
-        }
-
-        if (Input.GetKey("right"))
-        {
-            gameObject.transform.Translate(20f * Time.deltaTime, 0, 0);
-        }
 
         if (Input.GetButton("Fire1")&& Time.time > Ndisparo) 
         {
             Ndisparo = Time.time + tiempodisparo;
             Instantiate(disparo, posdisparo.position, Quaternion.identity);
         }
+    }
+
+    void FixedUpdate() 
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        MoverNave(h, v);
+        LimiteNave();
+        RotacionNave();
+    }
+
+    void MoverNave(float h, float v)
+    {
+        movimiento = new Vector3(h, 0.0f, v);
+        rigidNave.velocity = movimiento * velocidadNave;
+    }
+
+    void LimiteNave()
+    {
+        rigidNave.position = new Vector3(Mathf.Clamp(rigidNave.position.x,-7.8f,7.8f), 0.0f, Mathf.Clamp(rigidNave.position.z,-3f,14f));
+    }
+     void RotacionNave()
+    {
+        rigidNave.rotation = Quaternion.Euler(rigidNave.velocity.z * 1f, 0.0f, rigidNave.velocity.x * -1f);
     }
 }
